663ac73f1683f9f9b9f145206c13d393f9ea3789
DAME RAJEE <doss72180@gmail.com>
2024-08-16 08:30:37 +0000

    added processing-data

--- clip /modeling_clip.py
+++ clip /modeling_clip.py
@@ -1,2 +1,28 @@
 import torch 
-import transformers 
+import torch.nn as nn 
+import transformers
+from transformers import  PreTrainedModel
+import timm 
+
+VISION_ENCODER = "love"
+TEXT_ENCODER = "text-Love"
+
+
+class vision_encoder(nn.Module):
+    def __init__(self,vision_encoder,):
+        pass  
+
+class text_encoder(nn.Module):
+    def __init__(self,text_encoder):
+        pass  
+
+class MLP(nn.Module):
+    def __init__(self):
+        pass 
+
+
+class CLIP(PreTrainedModel):
+    def __init__(self,):
+        pass  
+
+
