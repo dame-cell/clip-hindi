99dec81d0233bbfa8a9fc5e686109e6e304d8846
DAME RAJEE <doss72180@gmail.com>
2024-08-20 15:05:21 +0000

    last change of code hopefully

--- clip/inference.py
+++ clip/inference.py
@@ -54,7 +54,7 @@ def get_image_embeddings(valid_df,image_path,model_path):
     valid_image_embeddings = []
     with torch.no_grad():
         for batch in tqdm(valid_loader):
-            image_features = model.image_encoder(batch["url"].to(CFG.device))
+            image_features = model.image_encoder(batch["image"].to(CFG.device))
             image_embeddings = model.image_projection(image_features)
             valid_image_embeddings.append(image_embeddings)
     return model, torch.cat(valid_image_embeddings)
